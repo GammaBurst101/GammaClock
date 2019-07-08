bab1359e2f80ff1543587aea0ff24508a5fd6b0f
Prasoon <prasoonpandey06@gmail.com>
2019-07-08 16:17:39 +0530

    Refactor Code

--- GammaClock/AlarmClock.java
+++ GammaClock/AlarmClock.java
@@ -1,122 +1,132 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Calendar;
 import javax.sound.sampled.*;
-import java.io.File;
+import java.io.*;
 
-public class AlarmClock 
+public class AlarmClock
 {
     private JFrame frame;
-    private JTextField time, alarmHour, alarmMin;
+    private JTextField timeDisplay, alarmHour, alarmMin;
     private JPanel panel;
-    private JButton b;
-    private JLabel l;
+    private JButton btn;
+    private JLabel txtL;
     private String alarm;
-    private Timer t, t2;
+    private Timer time, alarmChecker;
     private static boolean alarmRung = false;
-    
+    private AlarmSystem alarmSystem;
+
     public AlarmClock()
     {
         //Setting JFrame
         frame = new JFrame ("Clock");
         frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         frame.setResizable(true);
         frame.setLocationRelativeTo(null);
-        
+
         //initialisations and setting them
+
+        //alarm
+        alarmSystem = new AlarmSystem();
+
+        //background panel
         panel = new JPanel();
         panel.setLayout(new FlowLayout());
-        panel.setBounds(0 ,0, 500, 400);
-        
-        time = new JTextField(10);
-        time.setEditable(false);
-        time.setFont(new Font("Arial", Font.PLAIN, 48));
-        
-        alarmHour = new JTextField("HH");
-        alarmHour.setBounds(250, 200, 40, 40);
-        
-        alarmMin = new JTextField("MM");
-        alarmMin.setBounds(350, 200, 40, 40);
-        
-        b = new JButton("Set Alarm");
-        b.setBounds(250, 350, 100, 40);
-        b.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
+
+        //display for current time
+        timeDisplay = new JTextField(10);
+        timeDisplay.setEditable(false);
+        timeDisplay.setFont(new Font("Arial", Font.PLAIN, 48));
+
+        //input boxes for alarm time
+        alarmHour = new JTextField("HH");  
+        alarmMin = new JTextField("MM");  
+
+        //set/stop alarm button
+        btn = new JButton("Set Alarm");
+        btn.addActionListener(new ActionListener()
             {
-                if((b.getText().equals("Stop Alarm"))==false)
+                public void actionPerformed(ActionEvent e)
                 {
-                    alarm = alarmHour.getText()+":" + alarmMin.getText() +":0";
-                    b.setText("Stop Alarm");
+                    if((btn.getText().equals("Stop Alarm"))==false)
+                    {
+                        alarm = alarmHour.getText()+":" + alarmMin.getText() +":0";
+                        btn.setText("Stop Alarm");
+                    }
+                    else
+                    {
+                        alarm = "";
+                        btn.setText("Set Alarm");
+                    }
                 }
-                else
+            });
+
+        //label to prompt users
+        txtL = new JLabel("Enter the time to set alarm at here");
+
+        //timers for managing the current time and checking it
+        time = new Timer(1000,new TimeListener());
+        alarmChecker = new Timer(1000, new ActionListener(){
+                public void actionPerformed(ActionEvent e)
                 {
-                    alarm = "";
-                    b.setText("Set Alarm");
+                    if(timeDisplay.getText().equals(alarm))
+                        alarmSystem.ringAlarm();
+
+                    if(alarmRung==true && btn.getText().equals("Stop Alarm"))
+                    {
+                        btn.setText("Set Alarm");
+                        alarmRung = false;
+                    }
                 }
-            }
-        });
-        
-        l = new JLabel("Enter the time to set alarm at here");
-        l.setBounds(100, 200, 100, 40);
-        
-        t = new Timer(1000,new Listener());
-        t2 = new Timer(1000, new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
-                if(time.getText().equals(alarm))
-                ringAlarm();
-                
-                if(alarmRung==true && b.getText().equals("Stop Alarm"))
-                {
-                    b.setText("Set Alarm");
-                    alarmRung = false;
-                }
-            }
-        });
-        
+            });
+
         //Add components
-        panel.add(time);
-        panel.add(l);
+        panel.add(timeDisplay);
+        panel.add(txtL);
         panel.add(alarmHour);
         panel.add(alarmMin);
-        panel.add(b);
+        panel.add(btn);
         frame.add(panel);
-        
+
         //Starting the timers
-        t.start();
-        t2.start();
+        time.start();
+        alarmChecker.start();
+
+        //setting frame to visible in last so that the frame doesn't need refresh to show all components
+        frame.setVisible(true);
     }
-    
-    class Listener implements ActionListener
+
+    class TimeListener implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
             Calendar cal = Calendar.getInstance();
-            
+
             int hour = cal.get(Calendar.HOUR_OF_DAY);
             int min = cal.get(Calendar.MINUTE);
             int sec = cal.get(Calendar.SECOND);
-            
-            time.setText(hour+":"+min+":"+sec);
+
+            timeDisplay.setText(hour+":"+min+":"+sec);
         }
     }
-    
-    private static void ringAlarm()
-    {
-        try{
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("software\\sounds\\alarm.wav").getAbsoluteFile());
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.start();
-            alarmRung = true;
-        }catch(Exception e) {
-            System.out.println ("Error with playing sound.");
-            e.printStackTrace();
+
+    class AlarmSystem {
+        private void ringAlarm()
+        {
+            try{
+                InputStream audioSrc = getClass().getResourceAsStream("sounds\\alarm.wav");
+                InputStream bufferedInput = new BufferedInputStream(audioSrc);
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedInput);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+                alarmRung = true;
+            }catch(Exception e) {
+                System.out.println ("Error with playing sound.");
+                e.printStackTrace();
+            }
         }
     }
 }
